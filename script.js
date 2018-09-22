d5c77f921c5ff42ae34e8de4069eff27cf0888ef
Shanyi Liow <shanyiliow1107@gmail.com>
2018-09-22 20:03:18 +0100

    This is a new upload on 23/9

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+var body = document.getElementById("background");
 
-function setGradient() {
+var baseColour1 = document.getElementById("intcolor1").value;
+var baseColour2 = document.getElementById("intcolor2").value;
+
+// var intColour2 = document.querySelector(
+
+
+console.log(baseColour1);
+console.log(baseColour2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function setGradient(){
 	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
+	"linear-gradient(to right, "
+	+color1.value 
+	+ "," 
 	+ color2.value 
-	+ ")";
+	+")";
 
 	css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
+// Color1
+color1.addEventListener("input",setGradient);
 
-color2.addEventListener("input", setGradient);
+
+// Colour2 
+color2.addEventListener("input",setGradient);
+
+
+
+
+// color2.addEventListener("input",function(){
+// 	body.style.background = 
+// 	"linear-gradient(to right, "
+// 	+color1.value 
+// 	+ "," 
+// 	+ color2.value 
+// 	+")";
+// })
+

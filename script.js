6205ea7129811abbd413f0b5b3e5cc1ef18a7e3f
Shanyi Liow <shanyiliow1107@gmail.com>
2018-09-24 01:51:17 +0800

    Working Background Generator

--- script.js
+++ script.js
@@ -1,58 +1,19 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var body = document.getElementById("background");
+var body = document.getElementById("gradient");
 
-var baseColour1 = document.getElementById("intcolor1").value;
-var baseColour2 = document.getElementById("intcolor2").value;
-
-// var intColour2 = document.querySelector(
-
-
-console.log(baseColour1);
-console.log(baseColour2);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function setGradient(){
+function setGradient() {
 	body.style.background = 
-	"linear-gradient(to right, "
-	+color1.value 
-	+ "," 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
 	+ color2.value 
-	+")";
+	+ ")";
 
 	css.textContent = body.style.background + ";";
 }
 
-// Color1
-color1.addEventListener("input",setGradient);
+color1.addEventListener("input", setGradient);
 
-
-// Colour2 
-color2.addEventListener("input",setGradient);
-
-
-
-
-// color2.addEventListener("input",function(){
-// 	body.style.background = 
-// 	"linear-gradient(to right, "
-// 	+color1.value 
-// 	+ "," 
-// 	+ color2.value 
-// 	+")";
-// })
-
+color2.addEventListener("input", setGradient);
